dc4b15565d8e7cd0fecd5de5cabcd9bdd50880cd
nadir <nadirelserhid@gmail.com>
2024-01-13 20:18:00 +0300

    assingment 18 to 22 is finsh

---
+++ assignments_18_22/main.js
@@ -0,0 +1,61 @@
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 + 15 % 3 + 190 + 10 - 400); // 0
+
+
+
+// assginment 2 
+
+
+// let num = 3;
+
+// // Solution One
+// console.log(num + num); // 6
+
+// // Solution Two
+// console.log(+num +num ); // 6
+
+// // Soultion Three
+// console.log(Number(num)+Number(num)); // 6
+
+// // Soultion Four
+// console.log(num*num-num); // 6
+
+// // Solution Five
+// console.log(num*num/num+num); // 6
+
+// // Solution Six
+// console.log( true+true+true +num); // 6
+
+// assngiment 3
+
+let num = "10";
+
+// Solution One
+console.log( +num + +num); // 20
+
+// Solution Two
+console.log(Number(num) +Number(num)); // 20
+
+// Solution Three
+console.log(); // 20
+
+// Solution Four
+console.log("Write Your Code Here"); // 20
+
+//assnhiment 4
+
+let points = 10;
+
+points++;
+points++;
+points++;
+
+console.log(points); // 13
+
+points--;
+points--;
+points--;
+points--;
+points--;
+
+console.log(points); // 8;
